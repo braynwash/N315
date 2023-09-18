68157401af4ccca89fca8671a984dbe7aa615aef
braynwash <f.tzgeraldz@gmail.com>
2023-09-17 21:07:20 -0400

    Added JS- downloaded CORRECT jquery

---
+++ homework2/app/app.js
@@ -0,0 +1,14 @@
+import { addPageContent } from "../model/model.js";
+
+function initListeners() {
+    $("nav a").on("click", (e)=> {
+        e.preventDefault();
+        let btnID = e.currentTarget.id;
+        addPageContent(btnID);
+    })
+}
+
+$(document).ready(function () {
+    addPageContent("home");
+    initListeners();
+});
