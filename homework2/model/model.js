ba9f847f595aa5fa788d241fb2b361d9f08bd774
braynwash <f.tzgeraldz@gmail.com>
2023-09-17 21:56:59 -0400

    Added 4 pages + styling

--- homework2/model/model.js
+++ homework2/model/model.js
@@ -1,4 +1,4 @@
-var homeContent = `<div class="hero">
+var homeContent = `<div class="hero home">
 <div class="text">
 <p style="text-decoration:underline;">Article</p>
 <h1>iTravel's Top 10 places to<br>
@@ -7,7 +7,7 @@ var homeContent = `<div class="hero">
 <p>by Lorem Ipsum</p>
 </div>
 </div>
-<div class="articles">
+<div class="wrapper">
 <div class="header">
   <h1>Top stories</h1>
   <p style="text-decoration:underline;">View all</p>
@@ -16,7 +16,7 @@ var homeContent = `<div class="hero">
   <div class="article-card">
     <img src="images/card1.jpeg" alt="img">
     <p style="font-weight: bold;">Sept. 16</p>
-    <h4>All About Greece</h4>
+    <h4>My Travel Journey</h4>
     <p style="color:grey; line-height: 25px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam officiis neque dignissimos enim commodi consectetur.</p>
   </div>
   <div class="article-card">
@@ -42,9 +42,122 @@ var homeContent = `<div class="hero">
 <footer>
 <p>© 2023  iTravel. All Rights Reserved</p>
 </footer>`;
-var aboutContent = `<h1>About Page</h1>`;
-var blogContent = `<h1>Services Page</h1>`;
-var contactContent = `<h1>Contact Page</h1>`;
+var aboutContent = `<div class="hero about">
+<div class="text">
+<h1>iTravel
+</h1>
+<p>Explore your world, with us.</p>
+</div>
+</div>
+<div class="wrapper">
+<div class="header">
+  <h1>Who We Are</h1>
+  <p style="text-decoration:underline;">Read more</p>
+</div>
+<div class="learn-wrap">
+    <div class="learn-more">
+    <h1>iTravel: Explore your world,<br>
+    with us.</h1>
+    <p style="color:grey; line-height: 25px;">At iTravel, we believe in bringing<br> travel to everyone. We believe in <br> busting travel myths to help make <br> your next trip unforgettable.</p>
+    <div class=learnImg></div>
+    </div>
+</div>
+</div>
+<footer>
+<p>© 2023  iTravel. All Rights Reserved</p>
+</footer>`;
+var blogContent = `<div class="blogHero blog">
+<div class="text">
+<h1>Read our Blog
+</h1>
+<p>Read new articles by experts like you!</p>
+</div>
+</div>
+<div class="wrapper">
+<div class="header">
+  <h1>Top stories</h1>
+  <p style="text-decoration:underline;">View all</p>
+</div>
+<div class="cards-wrap">
+  <div class="article-card">
+    <img src="images/card1.jpeg" alt="img">
+    <p style="font-weight: bold;">Sept. 16</p>
+    <h4>My Travel Journey</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam officiis neque dignissimos enim commodi consectetur.</p>
+  </div>
+  <div class="article-card">
+    <img src="images/card2.jpeg" alt="img">
+    <p style="font-weight: bold;">Sept. 13</p>
+    <h4>All About Greece</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, repellendus.</p>
+  </div>
+  <div class="article-card">
+    <img src="images/card3.jpeg" alt="img">
+    <p style="font-weight: bold;">Sept. 10</p>
+    <h4>Travelling on a Budget: Van Life Edition</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum dolor sit amet consectetur.</p>
+  </div>
+  <div class="article-card">
+    <img src="images/card4.jpeg" alt="img">
+    <p style="font-weight: bold;">Sept. 6</p>
+    <h4>Your Hiking MUST-HAVES</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum necessitatibus aliquam praesentium dolore quidem.</p>
+  </div>
+</div>
+<div class="cards-wrap">
+  <div class="article-card">
+    <img src="images/card2.jpeg" alt="img">
+    <p style="font-weight: bold;">Sept. 4</p>
+    <h4>Best Airline Awards</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam officiis neque dignissimos enim commodi consectetur.</p>
+  </div>
+  <div class="article-card">
+    <img src="images/card4.jpeg" alt="img">
+    <p style="font-weight: bold;">Sept. 1</p>
+    <h4>My Time Studying Abroad in Finland</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, repellendus.</p>
+  </div>
+  <div class="article-card">
+    <img src="images/card1.jpeg" alt="img">
+    <p style="font-weight: bold;">Aug. 29</p>
+    <h4>Which U.S. state has the BEST hotels?</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum dolor sit amet consectetur.</p>
+  </div>
+  <div class="article-card">
+    <img src="images/card3.jpeg" alt="img">
+    <p style="font-weight: bold;">Aug. 26</p>
+    <h4>Staying Safe while Travelling Single</h4>
+    <p style="color:grey; line-height: 25px;">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum necessitatibus aliquam praesentium dolore quidem.</p>
+  </div>
+</div>
+</div>
+<footer>
+<p>© 2023  iTravel. All Rights Reserved</p>
+</footer>`;
+var contactContent = `<div class="blogHero contact">
+<div class="text">
+<h1>Contact us
+</h1>
+<p>Got a question? We've got answers!</p>
+</div>
+</div>
+<div class="wrapper">
+<div class="header">
+  <h1>Get in Touch</h1>
+  <p style="text-decoration:underline;">Leave a Message</p>
+</div>
+<div class="learn-wrap">
+    <div class="learn-more">
+    <h1>We'd love to hear you!</h1>
+    <p style="color:grey; line-height: 25px;">If you've got any questions, comments, <br> or concerns, we'd love to hear you! Just <br> leave your name, an email, and whatever <br> you'd like us to note. A representative will <br> get back at you in 1-2 business days!</p>
+    <div class=learnImg contactUs></div>
+    </div>
+    
+</div>
+</div>
+<footer>
+<p>© 2023  iTravel. All Rights Reserved</p>
+</footer>`;
 
 export function addPageContent(pageName) {
     let pageContentName = pageName + "Content";
