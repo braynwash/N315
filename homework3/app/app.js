4d4cfb26d5ea15d950e8a440973d4a33d4ca31e8
braynwash <f.tzgeraldz@gmail.com>
2023-09-24 11:18:56 -0400

    Installed jquery- added url nav logic

---
+++ homework3/app/app.js
@@ -0,0 +1,25 @@
+import { changePage } from "../model/model.js";
+
+function route() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", "");
+    console.log(pageID);
+    changePage(hashTag, pageID)
+}
+
+function initListeners() {
+    console.log("Hello");
+}
+
+function initUrlListeners() {
+    $(window).on("hashchange", route);
+    route();
+}
+
+$(document).ready(function () {
+    initListeners();
+    initUrlListeners();
+    
+})
+ 
+ 
---
+++ homework3/model/model.js
@@ -0,0 +1,12 @@
+export function changePage(hashTag, pageID) {
+    if (pageID == "" || pageID == "home") {
+        $.get(`pages/home.html`, (data) => {
+            $("#app").html(data);
+        })
+    } else {
+        $.get(`pages/${pageID}.html`, (data) => {
+            $("#app").html(data);
+    })
+}
+}
+
