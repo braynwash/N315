eb98fb6834b858b81cd9b2ea09af017302f508de
braynwash <f.tzgeraldz@gmail.com>
2023-10-08 23:47:09 -0400

    Log in states attempt

--- homework5/app/app.js
+++ homework5/app/app.js
@@ -1,11 +1,21 @@
 import { changePage } from "../model/model.js";
 
+let loggedIn = false;
+
 function route() {
     let hashTag = window.location.hash;
     let pageID = hashTag.replace("#", "");
     changePage(hashTag, pageID);
 }
  
+if (loggedIn==false) {
+    $("#log").on("click", function(){
+       $.get(`pages/login.html`, (data) => {
+        $("#app").html(data);
+        }) 
+    })
+}
+
 function initListeners() {
     console.log("Test");
 }
