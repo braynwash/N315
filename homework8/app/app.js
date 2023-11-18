e6e6a06447d8a3d81935218516dd1fc9a9c1a583
braynwash <f.tzgeraldz@gmail.com>
2023-11-18 17:11:06 -0500

    Added create form functionality

--- homework8/app/app.js
+++ homework8/app/app.js
@@ -1,34 +1,64 @@
-function changePage(hashTag, pageID) {
-    if (pageID == "" || pageID == "home") {
-        $.get(`pages/home.html`, (data) => {
-            $("#app").html(data);
-        })
-    } else {
-        $.get(`pages/${pageID}.html`, (data) => {
-            $("#app").html(data);
+import { addRecipe, changePage } from "./model.js";
+
+let initialIngred = 3;
+let initialInstruct = 3;
+
+function initListeners() {
+$("nav a").on("click", (e) => {
+    e.preventDefault();
+    let btnID = e.currentTarget.id;
+    changePage(btnID);
+    console.log("Click!");
+});
+}
+
+export function addFormListener() {
+    console.log("form listen");
+    $("#ingredient").on("click", (e) => {
+        initialIngred++;
+        $(".formIngred").append(
+            `<input type="text" placeholder="Ingredient ${initialIngred}" id="ingred${initialIngred - 1}">`
+        );
     })
-}
-}
+    $("#instruct").on("click", (e) => {
+        initialInstruct++;
+        $(".formInstruct").append(
+            `<input type="text" placeholder="Instruction ${initialInstruct}" id="instr${initialInstruct - 1}">`
+        );
+    })
+    $("#submit").on("click", (e) => {
+        let newItemObj = {};
 
-function route() {
-    let hashTag = window.location.hash;
-    let pageID = hashTag.replace("#", "");
-    changePage(hashTag, pageID);
-}
+        let imagePath = $("#imagePath").val();
+        let itemName = $("#itemName").val();
 
-var mobileBtn = document.getElementById("hamburger");
-
-mobileBtn.addEventListener("click", toggleMenu);
-
-function toggleMenu() {
-    mobileBtn.classList.toggle("open");
-}
-
-function initUrlListeners() {
-    $(window).on("hashchange", route);
-    route();
+        newItemObj.imagePath = imagePath;
+        newItemObj.itemName = itemName;
+        newItemObj.ingredients = [];
+        newItemObj.instructions = [];
+        console.log(newItemObj.imagePath);
+        console.log(newItemObj.itemName);
+        $('.formIngred input').each(function(index,data) {
+            var value = $(this).val();
+            let keyName = "ingredient" + index;
+            let ingredObj = {};
+            ingredObj[keyName] = value;
+            newItemObj.ingredients.push(ingredObj);
+            console.log(ingredObj);
+        })
+        $('.formInstruct input').each(function(index,data) {
+            var value = $(this).val();
+            let keyName = "instruction" + index;
+            let instructObj = {};
+            instructObj[keyName] = value;
+            newItemObj.instructions.push(instructObj);
+            console.log(instructObj);
+        });
+        addRecipe(newItemObj);
+    });
 }
 
 $(document).ready(function () {
-    initUrlListeners();
+    changePage("home");
+    initListeners();
 })
---
+++ homework8/app/model.js
@@ -0,0 +1,49 @@
+import { addFormListener } from "./app.js";
+
+var recipes = [];
+
+export function changePage(pageName) {
+    $("#app .formHolder div").off("click", "**");
+    $("#app .formHolder #submit").off("click", "**");
+    $.get(`pages/${pageName}.html`, (data) => {
+        $("#app").html(data);
+        addFormListener();
+        if(recipes.length == 0) {
+            $(".vr").append(`<p>You have no recipes.</p>`);
+        } else {
+        $.each(recipes, (idx, recipe) => {
+            $(".recipeHolder").append(`
+            <div class="recipeCard">
+            <div class="imgHolder">
+                <img src="${recipe.imagePath}" alt="">
+            </div>
+            <div class="titleDesc">
+                <h2>Awesome buny says: ${recipe.itemName}</h2>
+                <h3>Description</h3>
+                <p>Yummy yum carrot ipsum</p>
+                <div class="ingredients">
+                    <h2>Ingredients</h2>
+                    <ul>
+                    ${(() => { let htmlString = ""; $.each(recipe.ingredients, (idx, ingredient) => { let keyName = "ingredient" + idx; htmlString += `<li>${ingredient[keyName]}</li>`; }); 
+                    return htmlString; })()}
+                    </ul>
+                </div>
+                <div class="instructions">
+                    <h2>Instructions</h2>
+                    <ol>
+                    ${(() => { let htmlString = ""; $.each(recipe.instructions, (idx, instruction) => { let keyName = "instruction" + idx; htmlString += `<li>${instruction[keyName]}</li>`; }); 
+                    return htmlString; })()}
+                    </ol>
+                </div>
+            </div>
+           </div>`);
+        })}
+    }).fail((error) => {
+        console.log("error ", error);
+    });
+}
+
+export function addRecipe(newRecipe) {
+    recipes.push(newRecipe);
+    console.log(recipes);
+}
